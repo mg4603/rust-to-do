6c88ea93ae07aaf88def39f59b40ef77ea76f84b
michael <michael.g.george@gmail.com>
2025-12-29 01:04:28 +0530

    add Task struct to main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Task {
+    id: u32,
+    text: String,
+    done: bool,
+}
+
 fn main() {
     println!("Hello, world!");
 }
