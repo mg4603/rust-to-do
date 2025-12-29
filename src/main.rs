eab9f6934146cd3ad5e3a4658659b7a1a1e9d6e4
michael <michael.g.george@gmail.com>
2025-12-29 07:01:49 +0530

    add load_tasks fn to main

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use serde::{Deserialize, Serialize};
+use std::fs;
+use std::path::Path;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Task {
@@ -10,6 +12,17 @@ struct Task {
 #[derive(Serialize, Deserialize, Debug)]
 struct TaskList {
     tasks: Vec<Task>,
+}
+
+const FILE: &str = "tasks.json";
+
+fn load_tasks() -> TaskList {
+    if !Path::new(FILE).exists() {
+        return TaskList { tasks: Vec::new() };
+    }
+
+    let data = fs::read_to_string(FILE).expect("Failed to read file");
+    serde_json::from_str(&data).unwrap_or(TaskList { tasks: Vec::new() })
 }
 
 fn main() {
